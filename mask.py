0c626135f8563d3250f3337579a7d797f0845e8c
Alioune CISSE <60492447+Alioune-Cisse@users.noreply.github.com>
2022-09-01 23:01:30 +0000

    Delete mask.py

--- mask.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-def generate_square_subsequent_mask(sz: int) :
-    """Generates an upper-triangular matrix of -inf, with zeros on diag."""
-    return torch.triu(torch.ones(sz, sz) * float('-inf'), diagonal=1)
